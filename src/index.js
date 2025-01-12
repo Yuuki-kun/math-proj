5949ef8ce5e2763a15b89100b577422975b7aae9
Yuuki-kun <tongcongminhart@gmail.com>
2025-01-12 13:09:35 +0700

    u router

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <AuthProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </AuthProvider>
   // </React.StrictMode>
 );
